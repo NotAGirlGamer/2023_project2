ba12ae606c10eaf09f79bd42abf07a17825df9c5
elaina-werth <werthemw@gmail.com>
2023-12-11 14:13:52 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,4 +5,17 @@ def menu():
     #it displays a menu to the user
     #returns an integer for choice
     
-    pass
+    #display the menu
+    print("\nWelcome to the Contact Management system. What would you like to do?")
+    print ("1) Add a contact")
+    print("2) Modify a contact")
+    print("3) Display a contact")
+    print ("4) Display all contacts")
+    print ("5) Delete a contact")
+    print("6) Exit")
+        
+    #prompt the user for a choice
+    choice = input("Enter a choice: ")
+        
+    #return choice
+    return int(choice)
